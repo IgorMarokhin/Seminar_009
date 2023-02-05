0451b4b57cb438b2a1c4883c6e8bfe336bdcf6c2
IgorMarokhin <lawyer.mii222@gmail.com>
2023-02-05 13:29:39 +0300

    Семинар 9. Добавлено решение задачи 63.

---
+++ Seminar_009_Task_63/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от 1 до N.
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число N: ","Ошибка ввода!");
+FindSequence(1, num);
+Console.WriteLine(FindSequence(1, num));
+
+//////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+
+string FindSequence(int start, int end)
+{
+    if (start == end) return end.ToString();
+    else return start + " " + FindSequence(start + 1, end);
+}
