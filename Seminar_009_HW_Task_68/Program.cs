e946a3f34fe65e1c4f1e3adbd598f2974303bd58
IgorMarokhin <lawyer.mii222@gmail.com>
2023-02-05 14:59:08 +0300

    Семинар 9. Домашнее задание. Добавлено решение задачи 68.

---
+++ Seminar_009_HW_Task_68/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
+Console.Clear();
+int num1 = GetNumberFromUser("Введите целое положительное число M: ","Ошибка ввода!");
+int num2 = GetNumberFromUser("Введите целое положительное число N: ","Ошибка ввода!");
+FunctionAkkerman(num1, num2);
+Console.WriteLine($"M = {num1}, N = {num2} -> A(M, N) = {FunctionAkkerman(num1, num2)}");
+
+//////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+int FunctionAkkerman(int M, int N)
+{
+    if (M == 0) return N + 1;
+    else if (N == 0) return FunctionAkkerman(M - 1, 1);
+    else return FunctionAkkerman(M - 1, FunctionAkkerman(M, N - 1));
+}
