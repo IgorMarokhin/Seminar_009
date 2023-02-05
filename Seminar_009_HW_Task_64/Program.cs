9be22d25e66e786ab81a0e4eee4065904ccd57f0
IgorMarokhin <lawyer.mii222@gmail.com>
2023-02-05 14:33:50 +0300

    Семинар 9. Домашнее задание. Добавлено решение задачи 64.

---
+++ Seminar_009_HW_Task_64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от N до M.
+
+Console.Clear();
+int num = GetNumberFromUser("Введите целое число: ","Ошибка ввода!");
+FindSequence(num);
+Console.WriteLine($"N = {num} -> {FindSequence(num)}");
+
+//////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+
+string FindSequence(int N)
+{
+    if (N == 1) return "1";
+    else return (N + " " + FindSequence(N - 1));
+}
