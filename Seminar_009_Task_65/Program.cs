7c53cd2e121e4251f249a0dd017df265710ac921
IgorMarokhin <lawyer.mii222@gmail.com>
2023-02-05 13:33:15 +0300

    Семинар 9. Добавлено решение задачи 65.

---
+++ Seminar_009_Task_65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+
+Console.Clear();
+int num1 = GetNumberFromUser("Введите целое число M: ","Ошибка ввода!");
+Console.Clear();
+int num2 = GetNumberFromUser("Введите целое число N: ","Ошибка ввода!");
+FindSequence(num1, num2);
+Console.WriteLine(FindSequence(num1, num2));
+
+//////////////////////////////////
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+
+string FindSequence(int start, int end)
+{
+    if (start == end) return end.ToString();
+    else return start + " " + FindSequence(start + 1, end);
+}
